3602059bc4b0cef3970ac9237952059e09ab3740
vinh <86521877+meomoc207@users.noreply.github.com>
2021-07-05 19:14:08 +0700

    create test.html

---
+++ test.js
@@ -0,0 +1,8 @@
+function countdown() {
+    alert("sike you ain't gettin a countdown");
+    console.log('ha lol');
+}
+function clicked() {
+    console.log('a button is clicked');
+    alert('the button worked good job fam')
+}
